715892a9cc3cc070698750f12cae93492881dd75
wahyudwikrisnanto <wahyudwikrisnanto@gmail.com>
2020-05-05 17:22:58 +0700

    start

---
+++ index.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <script src="js/bootstrap.js"></script>
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <div class="container" style="margin-top:100px;">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card bg-dark" style="max-width:300px">
+                    <div class="card-header text-light text-center" style="font-weight:500; display:inline-block;">
+                        PROFILE
+                    </div>
+                    <div class="card-body">
+                        <img src="img/avatar.png" class="rounded-circle mx-auto" alt="Avatar" style="width:150px; display:block; border: 3px solid grey">
+                    </div>
+                    <div class="card-footer text-light text-center display-5" style="font-weight:200;">
+                        <p>Wahyu Dwi Krisnanto <br> Mahasiswa TI</p>
+                        <p style="font-weight:500;">Contacts</p>
+                        <a href="https://instagram.com/wdkris" target="_blank">
+                            <img src="icons/instagram.svg" alt="" style="width:24px;display:inline-block">
+                        </a> &nbsp
+                        <a href="mailto:wahyudwikrisnanto@outlook.com">
+                            <img src="icons/mail.svg" alt="" style="width:24px;display:inline-block">
+                        </a>
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-8">
+                <div class="">
+                    <div class="text-light text-left p-2" style="font-weight:500; border-bottom:solid 1px grey;">
+                        ABOUT
+                    </div>
+                    <div class="card-body text-light text-justify" style="font-weight:200;">
+                        <div class="row">
+                            <div class="col-md-6">
+                                Hi! Saya Wahyu Dwi Krisnanto, Mahasiswa jurusan TI semester 4 disalah satu perguruan tinggi negeri di Indonesia. Saya sangat tertarik dengan bidang teknologi, salah satunya pemrograman aplikasi berbasis web. Selain menekuni pemrograman web, saya juga tertarik
+                            </div>
+                            <div class="col-md-6">
+                                 dengan photography, bisa kalian lihat pada halaman pribadi Instagram saya pada bagian kontak, atau anda dapat mengirim email pada kontak untuk keperluan lainnya.
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="text-light p-2" style="font-weight:500; border-bottom:solid 1px grey;">
+                        COMPETENCE ON
+                    </div>
+                    <div class="card-body">
+                         <img src="icons/bootstrap.svg" alt="" style="width:75px;display:block-inline">  &nbsp &nbsp &nbsp
+                        <img src="icons/laravel.svg" alt="" style="width:75px;">  &nbsp &nbsp &nbsp
+                         <img src="icons/mysql.svg" alt="" style="width:75px;">  &nbsp &nbsp &nbsp
+                         <img src="icons/css.svg" alt="" style="width:50px;">  &nbsp &nbsp &nbsp
+                         <img src="icons/html.svg" alt="" style="width:50px;">  &nbsp &nbsp &nbsp
+                         <img src="icons/js.svg" alt="" style="width:50px;">  &nbsp
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
